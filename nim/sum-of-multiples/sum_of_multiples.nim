e30448789e580becf83765071d17a503ed20b6d3
Wésley Guimarães <wes@guima.dev>
2026-05-21 02:48:03 -0300

    ✨ (nim) solve sum-of-multiples

--- nim/sum-of-multiples/sum_of_multiples.nim
+++ nim/sum-of-multiples/sum_of_multiples.nim
@@ -1,2 +1,14 @@
+import std/sequtils
+
 proc sum*(limit: int, factors: openArray[int]): int =
-  discard
+  var valid: seq[int] = @[]
+  for factor in factors:
+    var mult = 0
+    while mult < limit and factor > 0:
+      if mult > 0: valid.addUnique(mult)
+      mult += factor
+  echo "valid.len = " & $valid.len
+  if valid.len > 0:
+    result += foldl(valid, a + b)
+  else:
+    result = 0
